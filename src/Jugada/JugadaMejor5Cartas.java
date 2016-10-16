7a467ee414bedc910a6680077b7ad899c6495f7f
Jota <Jota@DESKTOP-QNFL1R6>
2016-10-16 22:00:29 +0200

    ¿Corregido problema con staigh flush? Comprobadlo.

--- src/Jugada/JugadaMejor5Cartas.java
+++ src/Jugada/JugadaMejor5Cartas.java
@@ -7,6 +7,7 @@ import java.util.Vector;
 import Cartas.Card;
 import Cartas.Hand;
 import Cartas.Ranking;
+import Cartas.Suit;
 
 public class JugadaMejor5Cartas {
 	
@@ -38,6 +39,8 @@ public class JugadaMejor5Cartas {
 		boolean color = true;		// Boolean para comprobar si es del mismo color
 		int gutshot = 0;			// Numero de hueco del mano, gutshot <= 1 puede tener escalera
 		boolean straight = true;	// Boolean para comprobar si es una escalera
+		Suit colorAnterior = null;  // Contiene el coloranterior de la escalera
+		boolean straightColor = true;// Escalera de color?
 		int value = 0;				// Variable auxiliar para guardar valor de la carta anterio, para poder comparar despues
 		String palo = "";			// Variable auxiliar para guardar palo de la carta anterio, para poder comparar despues
 		
@@ -49,21 +52,34 @@ public class JugadaMejor5Cartas {
 		*/
 		int count2 = 0;
 		
-		
 		Iterator<Card> it = map.keySet().iterator();
 		while(it.hasNext()){
 			Card aux = it.next();
 			count++;
 			count2++;
 			
+			/* Esto es un poco feo, pero no se me ocurre nada ahora */
+			if(colorAnterior == null) colorAnterior = aux.getSuit();
+			
 			switch(map.get(aux)){
 			case 1:
+				/* Este era el principal problema, ponias straight a false en cuanto no se cumplia una vez,
+				 * y asi estabas evitando comprobar que apareciese una escalera despues.
+				 * 
+				 * Si hay diferencia de 1, volvemos a comrpobarla
+				 */
+				if(!straight && value - aux.getValue().getValue() == 1){
+					straight = true;
+				}
+				
 				if(bestValue == 0){		// Primera vuelta
 					bestValue = Ranking.HIGH_CARD.getValor();
 					palo = aux.getSuit().getPalo();
 					value = aux.getValue().getValue();
-					this.bestHand = Ranking.HIGH_CARD.getName() + " " + aux.getValue() + " (" + aux + ")";
+					this.bestHand = Ranking.HIGH_CARD.getName() + " " + aux.getValue() + " (" + aux + ")";					
 				} else if(straight || color){
+					
+					
 					
 					if(value - aux.getValue().getValue() > 2){
 						// Si la diferencia de valor de carta anterio y ahora es mayor que 2, no hay escalera ni gutshot
@@ -74,6 +90,12 @@ public class JugadaMejor5Cartas {
 						straight = false;
 						gutshot++;
 					}
+					/* Este else if es lo nuevo, creo que se entiende */
+					else if(value - aux.getValue().getValue() == 1){
+						if(colorAnterior != aux.getSuit()){
+							straightColor = false;
+						}
+					}				
 					
 					// Si el palo de la carta anterio es diferente que la de ahora, no hay color
 					if(!palo.equals(aux.getSuit().getPalo())){
@@ -82,7 +104,8 @@ public class JugadaMejor5Cartas {
 					
 					if(count == 5){
 						// caso 1+1+1+1+1
-						if(color && straight){
+						/* Ahora comprobamos con la variable straightColor */
+						if(straightColor && straight){
 								// color y straight son true, escalera de color
 								bestValue = Ranking.STRAIGHT_FLUSH.getValor();
 								this.bestHand = Ranking.STRAIGHT_FLUSH.getName() + " (" + this.hand + ")";
@@ -103,6 +126,8 @@ public class JugadaMejor5Cartas {
 				
 				break;
 			case 2:
+				count = 1;
+				
 				// two pair or pair
 				if(bestValue == 0){
 					
@@ -156,6 +181,7 @@ public class JugadaMejor5Cartas {
 				break;
 			case 3:
 				color = false;		// no puede ser color
+				count = 1;
 				
 				// full house or three of a kind
 				if(bestValue == 0){
@@ -193,6 +219,8 @@ public class JugadaMejor5Cartas {
 			case 4:
 				// Si entrado en caso 4 carta del mismo valor, solo puede ser poker
 				color = false;
+				count = 1;
+				
 				bestValue = Ranking.FOUR_OF_A_KIND.getValor();
 				this.bestHand = Ranking.FOUR_OF_A_KIND.getName()+ " " + aux.getValue();
 				if(count2 == 2){
@@ -206,6 +234,8 @@ public class JugadaMejor5Cartas {
 			default:
 				break;
 			}
+			
+			colorAnterior = aux.getSuit();
 		}
 		
 		// cuatro cartas diferentes y gutshot < 2, draw Straight Gutshot y mejor mano es peor que escalera
