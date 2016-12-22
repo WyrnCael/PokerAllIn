1b02fcb2462a75a3feaf83c5834dfe1c755640e2
WyrnCael <juanjpri@ucm.es>
2016-12-22 19:16:29 +0100

    Limpieza para Practica 4

--- src/Poker_All_In/Poker_All_In.java
+++ src/Poker_All_In/Poker_All_In.java
@@ -1,28 +1,8 @@
 package Poker_All_In;
 
-import java.awt.EventQueue;
-
-import Cards.Card;
-import Cards.Hand;
-import GUI.BoardGUI;
-import Game.PokerGame;
-
 public class Poker_All_In {
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					BoardGUI frame = new BoardGUI(new PokerGame());
-					frame.setVisible(true);
-					frame.pack();
-					frame.setSize(1025, 550);
-					frame.setResizable(false);
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		
 	}
 }
